794a52b9d8186fdc411ab6d4ade2b929607f7f33
Angelica Oliveira miranda <anolivei@c1r7p1.42sp.org.br>
2020-03-08 17:16:04 -0300

    mix

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,78 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+        */
+/*   By: anolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/01 04:15:20 by anolivei          #+#    #+#             */
-/*   Updated: 2020/03/01 20:04:15 by anolivei         ###   ########.fr       */
+/*   Created: 2020/01/28 19:24:29 by anolivei          #+#    #+#             */
+/*   Updated: 2020/03/04 18:22:57 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "ft_printf.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "ft_printf.h"
 
 static int	count_digits(int n)
 {
-	int		i;
+	int				n_digits;
 	unsigned int	num;
-	
-	i = 0;
+
+	n_digits = 0;
 	if (n < 0)
-	{
 		num = n * (-1);
-		i = 1;
-	}
 	else
 		num = n;
 	while (num > 0)
 	{
 		num = num / 10;
-		i++;
+		n_digits++;
 	}
-	return (i);
+	return (n_digits);
 }
 
-char	*ft_itoa(int n)
+static int	power(int n_digits)
 {
-	char		*itoa;
+	int p10;
+
+	p10 = 1;
+	while (n_digits > 0)
+	{
+		p10 = p10 * 10;
+		n_digits--;
+	}
+	return (p10);
+}
+
+char		*ft_itoa(int n)
+{
+	char			*itoa;
+	int				p10;
+	int				i;
 	unsigned int	num;
-	int		n_digits;
-	int		i;
-	
+
+	if (!(itoa = (char *)malloc((count_digits(n) + 2) * sizeof(char))))
+		return (NULL);
 	i = 0;
-	n_digits = count_digits (n);
-	itoa = (char *)malloc(sizeof(char) * (n_digits + 1));
-	if (itoa == 0)
-		return (0);
 	if (n < 0)
 	{
-		itoa[i] = '-';
-		num = n * (-1);
-		i++;
+		num = num * (-1);
+		itoa[i++] = '-';
 	}
 	else
 		num = n;
-	if (num >= 0  && num <= 9)
+	p10 = power(count_digits(n) - 1);
+	while (p10 > 0)
 	{
-		itoa[i] = num + 48;
-		num = num / 10;
-	}
-	else if (num > 0)
-	{
-		while (i < n_digits)
-		{
-			itoa[i] = num % 10 + 48;
-			i++;
-			ft_itoa(num / 10);
-
-		}
+		itoa[i++] = (num / p10 % 10 + 48);
+		p10 = p10 / 10;
 	}
 	itoa[i] = '\0';
 	return (itoa);
-}
-
-int	main(void)
-{
-	printf("%s\n", ft_itoa(158282));
 }
