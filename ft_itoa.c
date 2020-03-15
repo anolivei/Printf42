fee4390ffd6f1fd7437ff0fcdd94596956e75e71
Angelica <angelica@Angelicas-MacBook-Pro.local>
2020-03-15 17:05:25 -0300

    novoitoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: anolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 19:24:29 by anolivei          #+#    #+#             */
-/*   Updated: 2020/03/15 05:51:55 by anolivei         ###   ########.fr       */
+/*   Updated: 2020/03/15 17:03:10 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,27 @@ char		*ft_itoa(int n)
 {
 	char			*str;
 	int				len;
-	unsigned int	num1;
-	unsigned int	num2;
+	unsigned int	num;
 	
-	if (n < 0)
-		num1 = n * (-10);
-	else
-		num1 = n * 10;
-	len = n <= 0 ? 1: 0;
-	num2 = num1;
-	while ((num1 = num1 / 10) > 0)
+	num = n < 0 ? n * (-1) : n;
+	len = n <= 0 ? 1 : 0;
+	while (num > 0)
+	{
+		num = num / 10;
 		len++;
+	}
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == 0)
 		return (0);
+	str[len] = '\0';
+	num = n < 0 ? n * (-1) : n;
 	if (n < 0)
 		str[0] = '-';
-	str[len--] = '\0';
-	while ((num2 = num2 / 10) > 0)
-		str[len--] = (num2 % 10 + '0');
+	while (num  > 9)
+	{
+		str[len-- - 1] = ((num % 10) + '0');
+		num = num / 10;
+	}
+	str[len - 1] = ((num % 10) + '0');
 	return (str);
 }
