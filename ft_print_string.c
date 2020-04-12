7269b73804cc859a65f1b3d8de45b6c7543247f5
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2020-04-12 05:20:21 -0300

    norm_ok-print_int

--- ft_print_string.c
+++ ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/14 04:43:11 by anolivei          #+#    #+#             */
-/*   Updated: 2020/04/12 04:52:13 by anolivei         ###   ########.fr       */
+/*   Updated: 2020/04/12 05:17:49 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,10 +67,8 @@ static void	ft_print_string_putchar(char *str, t_flags flag)
 static int	ft_print_string_w_p(char *str, t_flags flag, int ret, int len)
 {
 	char	c;
-	int		i;
 
 	c = flag.zero == 1 ? '0' : ' ';
-	i = 0;
 	if (flag.justify > 0)
 		ft_print_string_putchar(str, flag);
 	if (flag.precision >= len)
@@ -93,30 +91,30 @@ static int	ft_print_string_w_p(char *str, t_flags flag, int ret, int len)
 	return (ret);
 }
 
-int			ft_print_string(char *s, t_flags flag, int ret, int i)
+int			ft_print_string(char *str, t_flags flag, int ret)
 {
-	if (s == NULL)
-		s = "(null)";
+	if (str == NULL)
+		str = "(null)";
 	if (flag.precision < 0)
-		flag.precision = ft_strlen(s);
+		flag.precision = ft_strlen(str);
 	if (flag.width == 0 && flag.precision == 0)
 	{
 		if (flag.dot == 0)
-			return (ft_putstr(s));
+			return (ft_putstr(str));
 		else
 			return (0);
 	}
 	if (flag.width > 0 && flag.precision == 0)
-		return (ft_print_string_w(s, flag, ret, ft_strlen(s)));
+		return (ft_print_string_w(str, flag, ret, ft_strlen(str)));
 	if (flag.width == 0 && flag.precision > 0)
 	{
-		while (i < flag.precision && s[i])
-			ft_putchar(s[i++]);
-		return (flag.precision < ft_strlen(s) ? flag.precision : ft_strlen(s));
+		ft_print_string_putchar(str, flag);
+		return (flag.precision < ft_strlen(str) ? flag.precision :
+				ft_strlen(str));
 	}
 	if (flag.width > 0 && flag.justify > 0 && flag.precision == 0)
-		return (ft_print_string_w_j(s, flag, ret, ft_strlen(s)));
+		return (ft_print_string_w_j(str, flag, ret, ft_strlen(str)));
 	if (flag.width > 0 && flag.precision > 0)
-		return (ft_print_string_w_p(s, flag, ret, ft_strlen(s)));
+		return (ft_print_string_w_p(str, flag, ret, ft_strlen(str)));
 	return (0);
 }
